83a8911739e1119741c48802a7123752cbd846f8
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2013-03-15 17:26:31 +0100

    Commented out test part of setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 git clone https://github.com/broadgsa/gatk-protected.git gatk-protected
 cd gatk-protected
+
 # Validated gatk-version
 git checkout 5e89f01e106cc916e088d1ab43e66321f133b34c
+
 ant
+mkdir ../lib
 cp dist/* ../lib/
 cd ..
 rm -rf gatk-protected
 
-sbt test
+# Uncomment this to run tests after setup
+# sbt test
