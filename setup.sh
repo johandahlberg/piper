698b3a31e26f082f8faba12675f85a2b4de3a0d3
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2013-04-16 15:31:14 +0200

    Download RNA-SeQC as a part of the setup

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ cp dist/* ../lib/
 cd ..
 rm -rf gatk-protected
 
+# Get RNA-SeQC
+wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/rnaseqc/RNA-SeQC_v1.1.7.jar --directory-prefix=resources/
+
 # Uncomment this to run tests after setup
 # sbt test
 sbt package
