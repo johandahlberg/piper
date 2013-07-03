18ef3d5b08d53d9e7fd319b6618baa8a40dc1679
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2013-07-03 12:01:41 +0200

    Allow multiple instances of same sample in...
    ... header read group.

--- src/main/scala/molmed/utils/BamUtils.scala
+++ src/main/scala/molmed/utils/BamUtils.scala
@@ -9,11 +9,11 @@ object BamUtils {
     def getSampleNameFromReadGroups(bam: File): String = {
         val samFileReader = new SAMFileReader(bam)
         val samHeader = samFileReader.getFileHeader()
-        val sampleNames = samHeader.getReadGroups().map(rg => rg.getSample())
+        val sampleNames = samHeader.getReadGroups().map(rg => rg.getSample()).toSet
         require(!sampleNames.isEmpty, "Couldn't find read groups in file: " + bam.getAbsolutePath() + ". This is required for the script to work.")
-        require(sampleNames.length == 1, "More than one sample in file: " + bam.getAbsolutePath() +
+        require(sampleNames.size == 1, "More than one sample in file: " + bam.getAbsolutePath() +
             ". Please make sure that there is only one sample per file in input.")
-        sampleNames(0)
+        sampleNames.toList(0)
     }
 
 }
