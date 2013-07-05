2fc692cca8f1493deb7f7d62e3346b70ca277004
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2013-07-05 11:06:15 +0200

    Changing rRNAtargets default to null

--- src/main/scala/molmed/queue/extensions/RNAQC/RNASeQC.scala
+++ src/main/scala/molmed/queue/extensions/RNAQC/RNASeQC.scala
@@ -6,37 +6,38 @@ import org.broadinstitute.sting.queue.function.CommandLineFunction
 
 class RNASeQC extends CommandLineFunction {
 
-    analysisName = "RNASeQC"
+  analysisName = "RNASeQC"
 
-    @Argument(doc = "String of format Sample ID|Bam File|Notes", shortName = "i", fullName = "input", required = true)
-    var input: String = _
+  @Argument(doc = "String of format Sample ID|Bam File|Notes", shortName = "i", fullName = "input", required = true)
+  var input: String = _
 
-    @Input(doc = "Reference file in fasta format.", shortName = "r", fullName = "reference", required = true)
-    var reference: File = _
-    
-    @Output(doc = "StdOut used for placeholding purposes", shortName = "place", fullName = "place_holder", required = true)
-    var placeHolderFile: File = _
+  @Input(doc = "Reference file in fasta format.", shortName = "r", fullName = "reference", required = true)
+  var reference: File = _
 
-    @Input(doc = "GTF File defining the transcripts (must end in .gtf)", shortName = "t", fullName = "transcripts", required = true)
-    var transcripts: File = _
+  @Output(doc = "StdOut used for placeholding purposes", shortName = "place", fullName = "place_holder", required = true)
+  var placeHolderFile: File = _
 
-    @Input(doc = "Output directory", shortName = "o", fullName = "output_dir", required = true)
-    var output: File = _
+  @Input(doc = "GTF File defining the transcripts (must end in .gtf)", shortName = "t", fullName = "transcripts", required = true)
+  var transcripts: File = _
 
-    @Argument(doc = "intervalFIle for rRNA loci (must end in .list). This is an alternative flag to the -BWArRNA flag.", shortName = "rRNA", fullName = "rRNA_targets", required = false)
-    var rRNATargets: File = _
+  @Input(doc = "Output directory", shortName = "o", fullName = "output_dir", required = true)
+  var output: File = _
 
-    @Argument(doc = "Perform downsampling to the given number of reads.", shortName = "d", fullName = "downsample", required = false)
-    var downsample: Int = -1
+  @Argument(doc = "interval file for rRNA loci (must end in .list). This is an alternative flag to the -BWArRNA flag.", shortName = "rRNA", fullName = "rRNA_targets", required = false)
+  var rRNATargets: File = null
 
-    val rRNATargetString = if (rRNATargets != null) " -rRNA " + rRNATargets.getAbsolutePath() + " " else ""
-    val downsampleString = if (downsample > 0) " -d " + downsample + " " else ""
+  @Argument(doc = "Perform downsampling to the given number of reads.", shortName = "d", fullName = "downsample", required = false)
+  var downsample: Int = -1
 
-    override def commandLine = "java -jar resources/RNA-SeQC_v1.1.7.jar " +
-        " -s " + input +
-        " -r " + reference +
-        " -t " + transcripts +
-        " -o " + output +
-        rRNATargetString +
-        downsampleString + " > " + placeHolderFile
+  val rRNATargetString: String = if (rRNATargets != null) " -rRNA " + rRNATargets.getAbsolutePath() + " " else ""
+  val downsampleString: String = if (downsample > 0) " -d " + downsample + " " else ""
+
+  override def commandLine = "java -jar resources/RNA-SeQC_v1.1.7.jar " +
+    " -s " + input +
+    " -r " + reference +
+    " -t " + transcripts +
+    rRNATargetString +
+    downsampleString +
+    " -o " + output +
+    " > " + placeHolderFile
 }
