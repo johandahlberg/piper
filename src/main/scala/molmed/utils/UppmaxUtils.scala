f8e8ed58a417544c6fcea72d675e7f0fb48dee06
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2013-11-25 17:11:30 +0100

    Trying with Seq of args

--- src/main/scala/molmed/utils/UppmaxUtils.scala
+++ src/main/scala/molmed/utils/UppmaxUtils.scala
@@ -4,31 +4,31 @@ import org.broadinstitute.sting.queue.function.CommandLineFunction
 
 class UppmaxUtils(uppmaxConfig: UppmaxConfig) { 
 
-  val clusterString = if(!uppmaxConfig.clusterName.isEmpty) " --cluster=" + uppmaxConfig.clusterName.get else ""  
+  val clusterString = if(!uppmaxConfig.clusterName.isEmpty) " -M " + uppmaxConfig.clusterName.get else ""  
   val qosFlag = if (!uppmaxConfig.uppmaxQoSFlag.isEmpty) " --qos=" + uppmaxConfig.uppmaxQoSFlag.get else ""
-  val projectBaseString = " -A " + uppmaxConfig.projId + " "  + qosFlag + clusterString
+  val projectBaseString = Seq("-A " + uppmaxConfig.projId, qosFlag, clusterString)
 
   // General arguments to non-GATK tools
   trait ExternalCommonArgs extends CommandLineFunction {
-    this.jobNativeArgs +:= "-p core -n 8 " + projectBaseString
-    this.memoryLimit = Some(24)
+    this.jobNativeArgs = Seq("-p core -n 8") ++ projectBaseString
+    this.memoryLimit = Some(64)
     this.isIntermediate = false
   }
 
   trait SixGbRamJobs extends CommandLineFunction {
-    this.jobNativeArgs +:= "-p core -n 2 " + projectBaseString
-    this.memoryLimit = Some(6)
+    this.jobNativeArgs = Seq("-p core -n 1") ++ projectBaseString
+    this.memoryLimit = Some(16)
     this.isIntermediate = false
   }
 
   trait NineGbRamJobs extends CommandLineFunction {
-    this.jobNativeArgs +:= "-p core -n 3 " + projectBaseString
-    this.memoryLimit = Some(9)
+    this.jobNativeArgs = Seq("-p core -n 2") ++ projectBaseString
+    this.memoryLimit = Some(16)
     this.isIntermediate = false
   }
   
   trait FatNode extends CommandLineFunction {
-    this.jobNativeArgs +:= "-p node -C fat" + projectBaseString
+    this.jobNativeArgs = Seq("-p core -n 6") ++ projectBaseString
     this.memoryLimit = Some(48)
     this.isIntermediate = false
   }
