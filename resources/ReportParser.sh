3ec04df512b59114188bc19418f1b7ac0ccee3e5
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-01-08 11:03:19 +0100

    ReportParser wrapper with correct path

--- resources/ReportParser.sh
+++ resources/ReportParser.sh
@@ -1,1 +1,1 @@
-java ${JAVA_TMP} -cp ../target/scala-2.9.2/piper_2.9.2-0.1-SNAPSHOT.jar:../lib/Queue.jar molmed.utils.ReportParser "$@"
+java ${JAVA_TMP} -cp target/scala-2.9.2/piper_2.9.2-0.1-SNAPSHOT.jar:lib/Queue.jar molmed.utils.ReportParser "$@"
