aa72ae155e1f55ff6dbe9339cc8c569f36b75b41
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2014-06-03 11:10:33 +0200

    Cleanup setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash -l
 
-#Load needed module for build on uppmax
-module load java/sun_jdk1.7.0_25
+########################################################
+# Utility script to download dependencies and install
+# Piper
+# Usage:
+#  /setup.sh <path to wher Piper should be installed>
+# Default install location is $HOME/Bin/Piper
+########################################################
+
+INSTALL_PREFIX=$1
+
+if [ ! -n "$1" ];
+then
+  INSTALL_PREFIX="$HOME/Bin/Piper"
+fi
 
 check_errs()
 {
@@ -19,26 +31,67 @@ check_errs()
   fi
 }
 
-git clone https://github.com/broadgsa/gatk-protected.git gatk-protected
-check_errs $? "git clone FAILED"
+download_and_install_gatk()
+{
+  git clone https://github.com/broadgsa/gatk-protected.git gatk-protected
+  check_errs $? "git clone FAILED"
 
-cd gatk-protected
+  cd gatk-protected
 
-# Validated gatk-version
-#git checkout 6bda5696664da40bc2baef4f4cb69e4ef1f86ce5
-#check_errs $? "git checkout FAILED"
+  # Validated gatk-version
+  git checkout 72492bb87544de91522cfa52ad16610a9ff85445
+  check_errs $? "git checkout FAILED"
 
-mvn package
-check_errs $? "gatk compilation FAILED"
-mkdir ../lib
-cp target/* ../lib/
-cd ..
-rm -rf gatk-protected
+  mvn package
+  check_errs $? "gatk compilation FAILED"
+  mkdir ../lib
+  cp target/* ../lib/
+  cd ..
+  rm -rf gatk-protected
+}
 
-# Get RNA-SeQC
-wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/rnaseqc/RNA-SeQC_v1.1.7.jar --directory-prefix=resources/
+echo "########################################################"
+echo "Setting up and installing Piper to: ${INSTALL_PREFIX}"
+echo "########################################################"
+
+#Load needed module for build on uppmax
+module load java/sun_jdk1.7.0_25
+
+if (( $? == 0 )); then
+  echo "Used module system to load Java (sun_jdk1.7.0_25)."
+else
+  echo "Couldn\'t load Java using a module system - but don't worry."
+  echo "As long as you have Java installed (preferably sun_jdk1.7.0_25)"
+  echo "you should be find anyway."
+fi
+
+echo "########################################################"
+echo "Checking out and compiling the GATK and Queue"
+echo "########################################################"
+
+download_and_install_gatk
+
+echo "########################################################"
+echo "Download RNA-SeQC"
+echo "########################################################"
+
+wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/rnaseqc/RNA-SeQC_v1.1.7.jar --directory-prefix=resources/ --no-clobber
 check_errs $? "wget RNA-SeQC FAILED"
 
-# Uncomment this to run tests after setup
-# sbt test
-sbt/bin/sbt pack
+echo "########################################################"
+echo "Compile, package and install Piper"
+echo "########################################################"
+
+sbt/bin/sbt pack && make -C target/pack/ install PREFIX=$INSTALL_PREFIX
+check_errs $? "compiling and install piper failed."
+
+echo "########################################################"
+echo "Piper successfully installed to ${INSTALL_PREFIX}"
+echo "Add it to your PATH by running:"
+echo "  PATH=$PATH:$INSTALL_PREFIX/bin"
+echo "And verify it's been installed by running:"
+echo "  piper -S qscripts/examples/NonUppmaxableTestScript.scala --help"
+echo "This should show a list of available options if Piper"
+echo " has been installed correctly."
+echo "########################################################"
+
