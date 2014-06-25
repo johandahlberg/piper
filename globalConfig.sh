aa0efb07b8900122454a2cbe9e2215e2be1fcb79
Johan Dahlberg <johan.dahlberg@medsci.u.se>
2014-06-25 10:10:17 +0200

    Correct drmaa lib location on uppmax

--- globalConfig.sh
+++ globalConfig.sh
@@ -1,10 +1,10 @@
 # Start by exporting the shared drmaa libaries to the LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/proj/b2010028/piper_resources/slurm-drmaa/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/sw/apps/build/slurm-drmaa/default/lib/:$LD_LIBRARY_PATH
 
 #---------------------------------------------
 # Check if we are running on uppmax or locally, and set the jobrunners and path accordingly
 #---------------------------------------------
-if [ -f "/proj/b2010028/piper_resources/slurm-drmaa/lib/libdrmaa.so" ];
+if [ -f "/sw/apps/build/slurm-drmaa/default/lib/libdrmaa.so" ];
 then
 	JOB_RUNNER=" Drmaa"
 	JOB_NATIVE_ARGS="-A ${PROJECT_ID} -p node -N 1 ${QOS}"
